a04c6267d76cdce715e811a949dfb51a183f6ea8
rahulkacha <rahulbkacha@gmail.com>
2023-02-01 18:59:30 +0530

    database connection is successful and can be read

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
-const sql = require("mssql");
+const sql = require("mysql2");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
@@ -14,6 +14,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+const dbConfig = {
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "testdb",
+  port: 3308,
+};
+//GET API
+app.get("/database", function (req, res) {
+  const connection = sql.createConnection(dbConfig);
+
+  connection.connect();
+
+  connection.query("SELECT * from Persons", (err, rows, fields) => {
+    if (err) throw err;
+
+    console.log("The solution is: ", rows);
+  });
+
+  connection.end();
 });
 
 app.listen(5000, () => {
