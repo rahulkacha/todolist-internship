8aa20a28a61a167c822ff224f80ecc96da887bc8
rahulkacha <rahulbkacha@gmail.com>
2023-02-01 14:47:38 +0530

    fixed file directory for ejs.

--- app.js
+++ app.js
@@ -16,34 +16,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-// config for your database
-
-const sqlConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  server: "localhost",
-  port: 3308,
-  pool: {
-    max: 10,
-    min: 0,
-    idleTimeoutMillis: 30000,
-  },
-  options: {},
-};
-
-sql.connect(sqlConfig, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    new sql.Request().query("select * from todolists", (err, result) => {
-      // ... error checks
-
-      console.dir(result);
-    });
-  }
-});
-
 app.listen(5000, () => {
   console.log("listening on port 5000.");
 });
